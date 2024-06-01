5674de07661b8980031da15d923746bdbfc9b899
Addarsh <addarsh.cherokie@gmail.com>
2024-06-02 00:04:37 +0530

    Added function name
    Renamed 'class' to 'className' per React component rules.

--- src/create-icp.js
+++ src/create-icp.js
@@ -6,10 +6,10 @@ function CreateICP() {
           id="industry"
           className="create-icp-filter container d-flex flex-column mt-4"
         >
-          <label for="industry" class="form-label">
+          <label for="industry" className="form-label">
             Industry
           </label>
-          <select class="form-select-sm">
+          <select className="form-select-sm">
             <option selected>N/A</option>
             <option value="Saas">SaaS</option>
             <option value="Real Estate">Real Estate</option>
@@ -21,10 +21,10 @@ function CreateICP() {
           id="region"
           className="create-icp-filter container d-flex flex-column mt-4"
         >
-          <label for="region" class="form-label">
+          <label for="region" className="form-label">
             Region
           </label>
-          <select class="form-select-sm">
+          <select className="form-select-sm">
             <option selected>N/A</option>
             <option value="AMER">AMER</option>
             <option value="EMEA">EMEA</option>
@@ -35,30 +35,38 @@ function CreateICP() {
           id="company-headcount"
           className="create-icp-filter container d-flex flex-column mt-4"
         >
-          <label for="company-headcount" class="form-label">
+          <label for="company-headcount" className="form-label">
             Company Headcount
           </label>
-          <select class="form-select-sm">
+          <select className="form-select-sm">
             <option selected>N/A</option>
-            <option value="AMER">1-10</option>
-            <option value="EMEA">10-50</option>
-            <option value="APAC">50-100</option>
+            <option value="1-10">1-10</option>
+            <option value="10-50">10-50</option>
+            <option value="50-100">50-100</option>
           </select>
         </div>
         <div
-          id="department-headcount"
+          id="function-headcount"
           className="create-icp-filter container d-flex flex-column mt-4"
         >
-          <label for="department-headcount" class="form-label">
-            Department Headcount
+          <label for="function-headcount" className="form-label">
+            Function Headcount
           </label>
-          <select class="form-select-sm">
+          <select className="form-select-sm">
+            <option selected>Function Name</option>
+            <option value="Marketing">Marketing</option>
+            <option value="Sales">Sales</option>
+            <option value="Engineering">Engineering</option>
+            <option value="IT">IT</option>
+            <option value="HR">HR</option>
+          </select>
+          <select className="form-select-sm mt-2">
             <option selected>N/A</option>
-            <option value="AMER">1-5</option>
-            <option value="EMEA">1-10</option>
-            <option value="APAC">1-20</option>
-            <option value="APAC">1-30</option>
-            <option value="APAC">1-40</option>
+            <option value="1-5">1-5</option>
+            <option value="1-10">1-10</option>
+            <option value="1-20">1-20</option>
+            <option value="1-30">1-30</option>
+            <option value="1-40">1-40</option>
           </select>
         </div>
       </div>
