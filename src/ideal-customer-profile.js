a38934f5a4e5eda08cf4cc64a5d8ad5fb8017e04
Addarsh <addarsh.cherokie@gmail.com>
2024-06-01 15:24:56 +0530

    Added ICP creation instructions and button
    Now will add form for ICP creation

--- src/ideal-customer-profile.js
+++ src/ideal-customer-profile.js
@@ -1,5 +1,27 @@
 function IdealCustomerProfile() {
-  return <div>Demo ICP page</div>;
+  return (
+    <div id="icp" className="container d-flex justify-content-center">
+      <div
+        id="instructions-div"
+        className="container d-flex flex-column m-3 p-3"
+      >
+        <div>
+          <p>
+            You can create the Ideal Customer Profile (ICPs) and Personas used
+            for your outbound efforts.
+          </p>
+          <p>
+            Multiple ICPs can be created and each one can be used in a different
+            campaign.
+          </p>
+          <p>ICP configurations can also be edited at any time.</p>
+        </div>
+        <button type="button" className="mt-2">
+          Create new ICP
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default IdealCustomerProfile;
