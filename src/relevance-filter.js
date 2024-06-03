729642d0ebb2548a694009ce03b76a36e1c67baa
Addarsh <addarsh.cherokie@gmail.com>
2024-06-03 13:58:09 +0530

    Added isHiring relevance filter as well
    Looks nice, will contiue adding the rest

--- src/relevance-filter.js
+++ src/relevance-filter.js
@@ -5,17 +5,52 @@ function RaisedFunding() {
   return (
     <div
       id="raised-funding-container"
-      className="container d-flex flex-row justify-content-start mt-3"
+      className="container d-flex flex-row justify-content-start mt-3 p-0"
     >
-      <div class="p-1">
+      <div class="p-0">
         <p>In the last </p>
       </div>
       <Form.Select className="ms-3">
         <option value="none" selected></option>
-        <option value="3months">3 months</option>
-        <option value="6months">6 months</option>
-        <option value="12months">12 months</option>
+        <option value="3m">3 months</option>
+        <option value="6m">6 months</option>
+        <option value="12m">12 months</option>
       </Form.Select>
+    </div>
+  );
+}
+
+function IsHiring() {
+  return (
+    <div
+      id="is-hiring-container"
+      className="container d-flex flex-column mt-3 p-0"
+    >
+      <div className="container d-flex flex-row p-0 is-hiring-inputs">
+        <p>Function</p>
+        <Form.Select className="ms-3">
+          <option value="any" selected>
+            Any
+          </option>
+          <option value="sales">Sales</option>
+          <option value="marketing">Marketing</option>
+          <option value="engineering">Engineering</option>
+          <option value="product">Product</option>
+          <option value="HR">HR</option>
+          <option value="IT">IT</option>
+        </Form.Select>
+      </div>
+      <div className="container d-flex flex-row p-0 is-hiring-inputs">
+        <p>Role</p>
+        <Form.Select className="ms-3">
+          <option value="any" selected>
+            Any
+          </option>
+          <option value="sdr">Sales Development Representative</option>
+          <option value="ae">Account Executive</option>
+          <option value="eng-manager">Engineering Manager</option>
+        </Form.Select>
+      </div>
     </div>
   );
 }
@@ -24,6 +59,8 @@ function RelevanceFilterInputs({ filterId }) {
   switch (filterId) {
     case "raised-funding-recently":
       return <RaisedFunding />;
+    case "hiring-recently":
+      return <IsHiring />;
     default:
       return null;
   }
