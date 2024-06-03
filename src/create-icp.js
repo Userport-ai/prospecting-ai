43bb3f9eddcbfcdd98db8aa3ccff5bdfca327c92
Addarsh <addarsh.cherokie@gmail.com>
2024-06-03 09:56:33 +0530

    Testing out relevance based filters
    Got static list of filters in select option, now have to render the correct components

--- src/create-icp.js
+++ src/create-icp.js
@@ -1,13 +1,66 @@
 import "./create-icp.css";
-import { Accordion, Button } from "react-bootstrap";
+import { Accordion, Button, Form, CloseButton, Card } from "react-bootstrap";
+
+function RelevanceFilter() {
+  return (
+    <Card className="mt-1">
+      <Card.Body>
+        <div
+          id="relevance-fitler-container"
+          className="container d-flex flex-row justify-content-between"
+        >
+          <Form.Select size="sm" className="w-90">
+            <option selected></option>
+            <option value="raised-funding-recently">
+              Have they raised money recently?
+            </option>
+            <option value="hiring-recently">
+              Have they started hiring recently?
+            </option>
+            <option value="similar-to-customers">
+              Are they similar to some of our current customers in the same
+              vertical?
+            </option>
+            <option value="using-competitors">
+              Are they using any of our competitors today?
+            </option>
+            <option value="using-technologies">
+              Are they using these specific technologies in their stack?
+            </option>
+            <option value="acquired-company-recently">
+              Have they acquired any companies recently?
+            </option>
+            <option value="new-leadership-joined-recently">
+              Has a new leader joined the company recently?
+            </option>
+            <option value="planning-launch-to-new-market">
+              Have they recently announced plans to enter a new market?
+            </option>
+            <option value="product-announcement">
+              Have they announced the launch of a new product?
+            </option>
+            <option value="got-acquired-recently">
+              Have they been acquired recently?
+            </option>
+          </Form.Select>
+          <CloseButton size="sm" />
+        </div>
+      </Card.Body>
+    </Card>
+  );
+}
 
 function CreateICP() {
+  function handleAddRevelanceFilter() {
+    console.log("Testing that relevance works!");
+  }
+
   return (
     <div className="d-flex flex-row justify-content-center">
       <div id="create-icp" className="d-flex flex-column mt-5 pb-3">
         <div
           id="account-profile-title"
-          className="create-icp-filter container d-flex justify-content-center mb-2"
+          className="create-icp-filter container d-flex justify-content-center mt-2 mb-2"
         >
           <p>Account Profile</p>
         </div>
@@ -19,13 +72,13 @@ function CreateICP() {
             <Accordion.Item eventKey="industry">
               <Accordion.Header>Industry</Accordion.Header>
               <Accordion.Body>
-                <select className="form-select-sm">
+                <Form.Select size="sm">
                   <option selected>N/A</option>
                   <option value="Saas">SaaS</option>
                   <option value="Real Estate">Real Estate</option>
                   <option value="Banking">Banking</option>
                   <option value="Media">Media & Entertainment</option>
-                </select>
+                </Form.Select>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -35,12 +88,12 @@ function CreateICP() {
             <Accordion.Item eventKey="region">
               <Accordion.Header>Region</Accordion.Header>
               <Accordion.Body>
-                <select className="form-select-sm">
+                <Form.Select size="sm">
                   <option selected>N/A</option>
                   <option value="AMER">AMER</option>
                   <option value="EMEA">EMEA</option>
                   <option value="APAC">APAC</option>
-                </select>
+                </Form.Select>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -50,12 +103,12 @@ function CreateICP() {
             <Accordion.Item eventKey="company-headcount">
               <Accordion.Header>Company Headcount</Accordion.Header>
               <Accordion.Body>
-                <select className="form-select-sm">
+                <Form.Select size="sm">
                   <option selected>N/A</option>
                   <option value="1-10">1-10</option>
                   <option value="10-50">10-50</option>
                   <option value="50-100">50-100</option>
-                </select>
+                </Form.Select>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -65,22 +118,22 @@ function CreateICP() {
             <Accordion.Item eventKey="function-headcount">
               <Accordion.Header>Function Headcount</Accordion.Header>
               <Accordion.Body>
-                <select className="form-select-sm">
+                <Form.Select size="sm">
                   <option selected>Function Name</option>
                   <option value="Marketing">Marketing</option>
                   <option value="Sales">Sales</option>
                   <option value="Engineering">Engineering</option>
                   <option value="IT">IT</option>
                   <option value="HR">HR</option>
-                </select>
-                <select className="form-select-sm mt-2">
+                </Form.Select>
+                <Form.Select size="sm" className="mt-2">
                   <option selected>N/A</option>
                   <option value="1-5">1-5</option>
                   <option value="1-10">1-10</option>
                   <option value="1-20">1-20</option>
                   <option value="1-30">1-30</option>
                   <option value="1-40">1-40</option>
-                </select>
+                </Form.Select>
               </Accordion.Body>
             </Accordion.Item>
           </Accordion>
@@ -93,7 +146,10 @@ function CreateICP() {
           id="relevance-btn"
           className="create-icp-filter container d-flex flex-column mt-1"
         >
-          <Button>Add filter</Button>
+          <RelevanceFilter />
+          <Button onClick={handleAddRevelanceFilter} className="mt-2">
+            Add filter
+          </Button>
         </div>
       </div>
     </div>
