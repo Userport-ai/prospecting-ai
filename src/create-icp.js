a20ab09eacd352bc8c5c72502df6c15feadab6a1
Addarsh <addarsh.cherokie@gmail.com>
2024-06-04 12:41:11 +0530

    Refactored Container class in CreateICP component
    No change in functionality

--- src/create-icp.js
+++ src/create-icp.js
@@ -60,103 +60,109 @@ function RelevanceFilter() {
 }
 
 function CreateICP() {
-  function handleAddRevelanceFilter() {
-    console.log("Testing that relevance works!");
-  }
-
   return (
-    <div className="d-flex flex-row justify-content-center">
+    <div
+      id="create-icp-outer-body"
+      className="container d-flex flex-row justify-content-center"
+    >
       <div id="create-icp" className="d-flex flex-column mt-5 pb-3">
         <div
-          id="account-profile-title"
-          className="create-icp-filter container d-flex justify-content-center mt-2 mb-2"
+          id="create-icp-filter"
+          className="container  d-flex flex-column align-items-center"
         >
-          <p>Account Profile</p>
-        </div>
-        <div className="create-icp-filter filters-title container mt-2">
-          <p>Standard Filters</p>
-        </div>
-        <div className="create-icp-filter container d-flex flex-column">
-          <Accordion alwaysOpen>
-            <Accordion.Item eventKey="industry">
-              <Accordion.Header>Industry</Accordion.Header>
-              <Accordion.Body>
-                <Form.Select size="sm">
-                  <option value="none">N/A</option>
-                  <option value="Saas">SaaS</option>
-                  <option value="Real Estate">Real Estate</option>
-                  <option value="Banking">Banking</option>
-                  <option value="Media">Media & Entertainment</option>
-                </Form.Select>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
-        <div className="create-icp-filter container d-flex flex-column mt-4">
-          <Accordion alwaysOpen>
-            <Accordion.Item eventKey="region">
-              <Accordion.Header>Region</Accordion.Header>
-              <Accordion.Body>
-                <Form.Select size="sm">
-                  <option value="none">N/A</option>
-                  <option value="AMER">AMER</option>
-                  <option value="EMEA">EMEA</option>
-                  <option value="APAC">APAC</option>
-                </Form.Select>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
-        <div className="create-icp-filter container d-flex flex-column mt-4">
-          <Accordion alwaysOpen>
-            <Accordion.Item eventKey="company-headcount">
-              <Accordion.Header>Company Headcount</Accordion.Header>
-              <Accordion.Body>
-                <Form.Select size="sm">
-                  <option value="none">N/A</option>
-                  <option value="1-10">1-10</option>
-                  <option value="10-50">10-50</option>
-                  <option value="50-100">50-100</option>
-                </Form.Select>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
-        <div className="create-icp-filter container d-flex flex-column mt-4">
-          <Accordion alwaysOpen>
-            <Accordion.Item eventKey="function-headcount">
-              <Accordion.Header>Function Headcount</Accordion.Header>
-              <Accordion.Body>
-                <Form.Select size="sm">
-                  <option value="none">Function Name</option>
-                  <option value="Marketing">Marketing</option>
-                  <option value="Sales">Sales</option>
-                  <option value="Engineering">Engineering</option>
-                  <option value="IT">IT</option>
-                  <option value="HR">HR</option>
-                </Form.Select>
-                <Form.Select size="sm" className="mt-2">
-                  <option>N/A</option>
-                  <option value="1-5">1-5</option>
-                  <option value="1-10">1-10</option>
-                  <option value="1-20">1-20</option>
-                  <option value="1-30">1-30</option>
-                  <option value="1-40">1-40</option>
-                </Form.Select>
-              </Accordion.Body>
-            </Accordion.Item>
-          </Accordion>
-        </div>
+          <div
+            id="account-profile-title"
+            className="container d-flex justify-content-center mt-2 mb-2"
+          >
+            <p>Account Profile</p>
+          </div>
+          <div className="filters-title container mt-2">
+            <p>Standard Filters</p>
+          </div>
+          <div className="container d-flex flex-column">
+            <Accordion alwaysOpen>
+              <Accordion.Item eventKey="industry">
+                <Accordion.Header>Industry</Accordion.Header>
+                <Accordion.Body>
+                  <Form.Select size="sm">
+                    <option value="none">N/A</option>
+                    <option value="Saas">SaaS</option>
+                    <option value="Real Estate">Real Estate</option>
+                    <option value="Banking">Banking</option>
+                    <option value="Media">Media & Entertainment</option>
+                  </Form.Select>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <div className="container d-flex flex-column mt-4">
+            <Accordion alwaysOpen>
+              <Accordion.Item eventKey="region">
+                <Accordion.Header>Region</Accordion.Header>
+                <Accordion.Body>
+                  <Form.Select size="sm">
+                    <option value="none">N/A</option>
+                    <option value="AMER">AMER</option>
+                    <option value="EMEA">EMEA</option>
+                    <option value="APAC">APAC</option>
+                  </Form.Select>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <div className="container d-flex flex-column mt-4">
+            <Accordion alwaysOpen>
+              <Accordion.Item eventKey="company-headcount">
+                <Accordion.Header>Company Headcount</Accordion.Header>
+                <Accordion.Body>
+                  <Form.Select size="sm">
+                    <option value="none">N/A</option>
+                    <option value="1-10">1-10</option>
+                    <option value="10-50">10-50</option>
+                    <option value="50-100">50-100</option>
+                  </Form.Select>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
+          <div className="container d-flex flex-column mt-4">
+            <Accordion alwaysOpen>
+              <Accordion.Item eventKey="function-headcount">
+                <Accordion.Header>Function Headcount</Accordion.Header>
+                <Accordion.Body>
+                  <Form.Select size="sm">
+                    <option value="none">Function Name</option>
+                    <option value="Marketing">Marketing</option>
+                    <option value="Sales">Sales</option>
+                    <option value="Engineering">Engineering</option>
+                    <option value="IT">IT</option>
+                    <option value="HR">HR</option>
+                  </Form.Select>
+                  <Form.Select size="sm" className="mt-2">
+                    <option>N/A</option>
+                    <option value="1-5">1-5</option>
+                    <option value="1-10">1-10</option>
+                    <option value="1-20">1-20</option>
+                    <option value="1-30">1-30</option>
+                    <option value="1-40">1-40</option>
+                  </Form.Select>
+                </Accordion.Body>
+              </Accordion.Item>
+            </Accordion>
+          </div>
 
-        <div className="create-icp-filter filters-title container mt-5">
-          <p>Relevance Filters</p>
-        </div>
-        <div
-          id="relevance-btn"
-          className="create-icp-filter container d-flex flex-column"
-        >
-          <Button onClick={handleAddRevelanceFilter}>Add filter</Button>
-          <RelevanceFilter />
+          <div
+            id="relevance-filter-id"
+            className="filters-title container d-flex mt-5"
+          >
+            <div className="d-flex flex-column justify-content-center">
+              Relevance Filters
+            </div>
+            <Button>+</Button>
+          </div>
+          <div className="container d-flex flex-column">
+            <RelevanceFilter />
+          </div>
         </div>
       </div>
     </div>
