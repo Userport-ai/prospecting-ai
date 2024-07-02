6d4bdeba05d45c9fdec617bd4762ca8b7ac023ca
Addarsh <addarsh.cherokie@gmail.com>
2024-07-02 21:59:21 +0530

    Refactored card info and added date
    Much nicer now

--- src/lead-result.js
+++ src/lead-result.js
@@ -14,60 +14,85 @@ It’s so cool that you can verify a user’s identity just by using their phone
 ];
 
 const aboutPerson = {
+  title: "About",
   text: "Zacahary is the CEO and Cofounder of Plaid. He cofounded the company 12 years ago in 2012. He studied at Duke university and worked at Bain & Company before that.",
   citationLink: "https://www.linkedin.com/in/zach-perret",
+  date: "Jul 1, 2024",
 };
 
 const thoughts = {
+  title: "Thoughts",
   text: "While introducing Plaid Consumer report he said, 'Credit underwriting in the US is broken. Too much paperwork, incomplete data, thin files, and many of the crucial consumer factors are not considered. Excited to be taking a first step towards fixing it'",
   citationLink:
     "https://www.linkedin.com/posts/zperret_introducing-consumer-report-a-new-solution-activity-7203797435322621953-mbS6",
+  date: "June 20, 2024",
 };
 
 const events = {
+  title: "Events Attended",
   text: "Talked about talking at Plaid's annual conference Plaid Effects. He said, 'This coming Tuesday, during Plaid Effects, we'll be sharing our latest product releases that are helping our customers onboard users faster, lower costs of payments and approve more borrowers while reducing risk.\
         We will also welcome guests from industry leaders like Coinbase, H&R Block and Adyen, who will share how they’re using Plaid to move the industry forward.'",
   citationLink:
     "https://www.linkedin.com/posts/zperret_effects-2024-activity-7207132820387893248--1N-/",
+  date: "June 13, 2024",
 };
 
-const fundraise = {
+const financials = {
+  title: "Financials",
   text: "Plaid has recently been in the news due to significant financial activity. As of June 2024, Plaid is raising funds at a valuation of $15 billion. This news comes as employees were offered shares at $1,200 each, indicating strong investor confidence in the company's future prospects. The fundraising efforts are part of Plaid's strategy to expand its financial technology services and continue its growth trajectory​.",
   citationLink:
     "https://www.fintechfutures.com/2021/01/plaid-raising-at-15bn-as-employees-pitched-1200-per-share/",
+  date: "Jan 2, 2024",
 };
 
 const awards = {
+  title: "Awards and Recognition",
   text: "We’re thrilled to be named #1 for the Finance category of  @FastCompany’s Most Innovative Companies for 2024! In 2023, we made a big push into payments, helping our customers access faster payments via new rails (RTP & FedNow) and keeping the ecosystem safe at the same time with anti-fraud tools like Signal. ",
   citationLink: "https://x.com/Plaid/status/1770101238379516302",
+  date: "March 10, 2024",
 };
 
 const announcements = {
+  title: "Company Announcements",
   text: "Plaid is proud to announce its strategic collaboration with Western Union, a global leader in cross-border and cross-currency payments.",
   citationLink: "https://plaid.com/blog/westernunion-europe-launch",
+  date: "May 12, 2024",
 };
 
 const productUpdates = {
+  title: "Product Updates",
   text: "Introducing Plaid Layer: The future of secure instant financial experiences. Plaid Layer is a new platform for secure, instant experiences that transforms how people use financial services starting with onboarding.",
   citationLink: "https://plaid.com/blog/introducing-plaid-layer",
+  date: "June 3, 2024",
 };
 
-function InfoCard({ text, citationLink = null }) {
+function OutreachCard({ text }) {
   return (
     <Card>
-      <Text>{text}</Text>
-      <div>
-        {citationLink && (
-          <Button type="link" href={citationLink}>
-            {citationLink}
-          </Button>
-        )}
-      </div>
+      <Text className="outreach-text">{text}</Text>
     </Card>
   );
 }
 
-function InfoSection({ sectionTitle, cardInfo = null }) {
+function InfoCard({ cardInfo }) {
+  return (
+    <Card>
+      <Text className="card-text">{cardInfo.text}</Text>
+      <div>
+        <Button
+          className="card-citation-link"
+          type="link"
+          href={cardInfo.citationLink}
+        >
+          {cardInfo.citationLink}
+        </Button>
+      </div>
+      <Text className="card-date">{cardInfo.date}</Text>
+    </Card>
+  );
+}
+
+function InfoSection({ cardInfo }) {
   const [cardClicked, setCardClicked] = useState(false);
   return (
     <>
@@ -76,11 +101,9 @@ function InfoSection({ sectionTitle, cardInfo = null }) {
         type="primary"
         onClick={() => setCardClicked(!cardClicked)}
       >
-        {sectionTitle}
+        {cardInfo.title}
       </Button>
-      {cardClicked && (
-        <InfoCard text={cardInfo.text} citationLink={cardInfo.citationLink} />
-      )}
+      {cardClicked && <InfoCard cardInfo={cardInfo} />}
     </>
   );
 }
@@ -95,39 +118,18 @@ function LeadResult() {
           <Title level={3}>Zach Perret, CEO, Plaid</Title>
         </Flex>
         <Flex id="info-container" vertical={true} gap="large">
-          <InfoSection
-            sectionTitle="About"
-            cardInfo={aboutPerson}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Thoughts"
-            cardInfo={thoughts}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Events Attended"
-            cardInfo={events}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Fundraise Announcements"
-            cardInfo={fundraise}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Awards and Recognition"
-            cardInfo={awards}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Company Announcements"
-            cardInfo={announcements}
-          ></InfoSection>
-          <InfoSection
-            sectionTitle="Product Updates"
-            cardInfo={productUpdates}
-          ></InfoSection>
+          <InfoSection cardInfo={aboutPerson}></InfoSection>
+          <InfoSection cardInfo={thoughts}></InfoSection>
+          <InfoSection cardInfo={events}></InfoSection>
+          <InfoSection cardInfo={financials}></InfoSection>
+          <InfoSection cardInfo={awards}></InfoSection>
+          <InfoSection cardInfo={announcements}></InfoSection>
+          <InfoSection cardInfo={productUpdates}></InfoSection>
         </Flex>
         <Flex id="outreach-container" vertical={true} gap="large">
           <Title level={4}>Sample Outreach Messages</Title>
-          <InfoCard text={outreachMessages[0]} />
-          <InfoCard text={outreachMessages[1]} />
+          <OutreachCard text={outreachMessages[0]} />
+          <OutreachCard text={outreachMessages[1]} />
         </Flex>
       </div>
     </div>
