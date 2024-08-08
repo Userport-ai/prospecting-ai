6fca8c1b75c803165f968d48a27bfd8b84afc256
Addarsh <addarsh.cherokie@gmail.com>
2024-08-08 17:32:01 +0530

    Implemented polling in Leads page
    Tested and it works with backend.
    
    Moved mock leads data to separate file.

--- src/leads-table.js
+++ src/leads-table.js
@@ -63,45 +63,6 @@ const columns = [
   },
 ];
 
-export const emptyLeadsResult = {
-  status: "success",
-  leads: [],
-};
-
-// Mock data from server.
-export const leadsResult = {
-  status: "success",
-  leads: [
-    {
-      id: "66ab9633a3bb9048bc1a0be5",
-      creation_date: null,
-      last_updated_date: null,
-      person_linkedin_url: "https://www.linkedin.com/in/zperret",
-      person_profile_id: null,
-      company_profile_id: null,
-      person_name: "Zachary Perret",
-      company_name: "Plaid",
-      person_role_title: "Co-Founder / CEO",
-      status: "complete",
-      company_headcount: 1222,
-      company_industry_categories: [
-        "banking",
-        "finance",
-        "financial-services",
-        "fintech-e067",
-        "insurtech",
-        "software",
-        "wealth-management",
-      ],
-      search_results_map: null,
-      report_creation_date_readable_str: null,
-      report_publish_cutoff_date: null,
-      report_publish_cutoff_date_readable_str: null,
-      details: null,
-    },
-  ],
-};
-
 function LeadsTable({ leads }) {
   const navigate = useNavigate();
   return (
--- src/leads.js
+++ src/leads.js
@@ -1,12 +1,34 @@
 import "./leads.css";
 import { Typography, Button, Spin } from "antd";
-import LeadsTable, { emptyLeadsResult } from "./leads-table";
-import { leadsResult } from "./leads-table";
+import LeadsTable from "./leads-table";
+import {
+  emptyLeadsResult,
+  leadsInProgressResult,
+  leadsResult,
+} from "./leads-table-data";
 import { useNavigate, useLoaderData, useNavigation } from "react-router-dom";
+import { useContext, useEffect, useState } from "react";
+import { AuthContext } from "./root";
 
 const { Title } = Typography;
 
-// Loader to fetch leads.
+// Helper to fetch list of leads created by this user.
+// TODO: Implement pagination.
+async function fetch_leads(user) {
+  const response = await fetch("/api/v1/leads", {
+    headers: { Authorization: "Bearer " + user.accessToken },
+  });
+  const result = await response.json();
+  // const result = await emptyLeadsResult;
+  // const result = await leadsResult;
+  // const result = await leadsInProgressResult;
+  if (result.status === "error") {
+    throw result;
+  }
+  return result.leads;
+}
+
+// Loader to fetch leads before component mounts.
 export const leadsLoader = (authContext) => {
   return async () => {
     const { user } = authContext;
@@ -14,30 +36,49 @@ export const leadsLoader = (authContext) => {
       // User is logged out.
       return null;
     }
-    const response = await fetch("/api/v1/leads", {
-      headers: { Authorization: "Bearer " + user.accessToken },
-    });
-    const result = await response.json();
-    // const result = await emptyLeadsResult;
-    // console result = await leadsResult;
-    if (result.status === "error") {
-      throw result;
-    }
-    return result;
+    return fetch_leads(user);
   };
 };
 
+// Fetch and display leads.
+// TODO: Implement pagination.
 function Leads() {
+  const { user } = useContext(AuthContext);
   const navigate = useNavigate();
-  const leadsResult = useLoaderData();
+  const gotLeads = useLoaderData();
+  const [leads, setLeads] = useState(gotLeads);
+
   const navigation = useNavigation();
   const loading_or_submitting = navigation.state !== "idle";
+  const should_poll_periodically = leads.some(
+    (lead) => !["complete", "failed_with_errors"].includes(lead.status)
+  );
+  const POLLING_INTERVAL = 60 * 1000; // Poll every 1 min.
+
+  // Setup polling.
+  useEffect(() => {
+    if (!should_poll_periodically) {
+      // Do nothing.
+      return;
+    }
+
+    const intervalId = setInterval(async () => {
+      const leads = await fetch_leads(user);
+      setLeads(leads);
+    }, POLLING_INTERVAL);
+    return () => clearInterval(intervalId);
+  }, [should_poll_periodically, user, POLLING_INTERVAL]);
+
+  if (!user) {
+    // User not logged in, return.
+    return null;
+  }
 
   return (
     <div id="leads-outer">
       <div id="leads-container">
         <Title level={3}>Leads</Title>
-        <LeadsTable leads={leadsResult.leads} />
+        <LeadsTable leads={leads} />
         <div id="add-leads-btn-container">
           <Button type="primary" onClick={() => navigate("/enter-lead-info")}>
             Add new lead
