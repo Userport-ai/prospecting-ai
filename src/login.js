ded8be85ca17080462b6a1832963d9cb358cdaee
Addarsh <addarsh.cherokie@gmail.com>
2024-08-16 22:09:17 +0530

    Fixed 'Sign in using Google' flow
    'redirect' flow was not working, so using popup flow instead.

--- src/login.js
+++ src/login.js
@@ -25,11 +25,28 @@ function Login() {
   useEffect(() => {
     // This will mount the Firebase Sign In UI and allow user to sign in using given options.
     var uiConfig = {
+      callbacks: {
+        // Called when the user has been successfully signed in.
+        signInSuccessWithAuthResult: function (authResult, redirectUrl) {
+          // Do not redirect. The login page in the app will automatically redirect.
+          // The authResult contains user object but we don't need it because we also listen
+          // to the onAuthStateChanged event to fetch User object whenever there is a sign in or
+          // sign out event.
+          // One example of information you cannot find other places is new user.
+          return false;
+        },
+        signInFailure: function (error) {
+          // Throw error so we understand what happened.
+          console.log("failed to sign in user with error: ", error);
+          throw error;
+        },
+      },
+      // Sign In flow does not seem to work on localhost if not used in popup mode, don't know why.
+      signInFlow: "popup",
       signInOptions: [
         firebase.auth.EmailAuthProvider.PROVIDER_ID,
         firebase.auth.GoogleAuthProvider.PROVIDER_ID,
       ],
-      signInSuccessUrl: "/leads",
       // TODO: Change this.
       tosUrl: "https://www.example.com/terms-conditions",
       privacyPolicyUrl: function () {
