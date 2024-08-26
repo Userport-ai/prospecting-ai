1d04f4296329968d53d928b5564dea63cd7f69ee
Addarsh <addarsh.cherokie@gmail.com>
2024-08-26 14:29:14 +0530

    Added flask app logger to Celery logger as well
    Tested on cloud and things look good finally.

--- api/flask_app/app/make_celery.py
+++ api/flask_app/app/make_celery.py
@@ -1,6 +1,15 @@
-from app import create_app
+import celery.signals
+from app import create_app, load_env_vars, setup_logging
 
 # This module is needed to initialize Celery worker from command line and access Celery App object.
+
+
+@celery.signals.setup_logging.connect
+def config_loggers(*args, **kwags):
+    """Setup logging for Celery app to match Flask app."""
+    load_env_vars()
+    setup_logging()
+
 
 flask_app = create_app()
 celery_app = flask_app.extensions["celery"]
