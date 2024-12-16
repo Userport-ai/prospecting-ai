f9a43141eafe14cecd003543cb2c470d11c601ae
add4user <addarsh@userport.ai>
2024-12-16 21:01:32 +0530

    Updated theme of app to Userport's purple theme
    Was able to update header with logo like v1
    
    Looks nice

--- frontend_v2/src/Header.jsx
+++ frontend_v2/src/Header.jsx
@@ -4,19 +4,29 @@ import {
   DropdownMenuContent,
   DropdownMenuItem,
 } from "./components/ui/dropdown-menu";
+import logo from "./assets/combination_mark_complementary.png";
+import { useNavigate } from "react-router";
 
 function Header() {
+  const navigate = useNavigate();
+
   return (
-    <header className="bg-white w-full shadow-md border-b border-gray-200">
-      <div className="w-full mx-0 flex items-center justify-between py-4 px-8">
-        {/* Logo */}
-        <div className="text-2xl font-bold text-gray-800">Userport</div>
+    <header className="bg-primary w-full shadow-md border-b border-gray-200">
+      <div className="w-full mx-0 flex items-center justify-between py-4 px-6">
+        <div className="w-44 h-11 bg-green-800 -mt-36">
+          <img
+            className="hover:cursor-pointer"
+            src={logo}
+            alt="userport-complementary-logo"
+            onClick={() => navigate("/accounts")}
+          />
+        </div>
 
         {/* Profile Dropdown */}
         <DropdownMenu>
           <DropdownMenuTrigger asChild>
-            <button className="flex items-center space-x-2 border border-gray-300 rounded-md px-3 py-1">
-              <span className="text-gray-700">Profile</span>
+            <button className="flex items-center space-x-2 border rounded-md px-3 py-1">
+              <span className="text-primary-foreground">Profile</span>
               <svg
                 xmlns="http://www.w3.org/2000/svg"
                 className="h-4 w-4"
@@ -34,10 +44,16 @@ function Header() {
             </button>
           </DropdownMenuTrigger>
           <DropdownMenuContent>
-            <DropdownMenuItem onClick={() => alert("Account Settings")}>
+            <DropdownMenuItem
+              className="hover:cursor-pointer"
+              onClick={() => alert("Account Settings")}
+            >
               Account Settings
             </DropdownMenuItem>
-            <DropdownMenuItem onClick={() => alert("Sign Out")}>
+            <DropdownMenuItem
+              className="hover:cursor-pointer"
+              onClick={() => alert("Sign Out")}
+            >
               Sign Out
             </DropdownMenuItem>
           </DropdownMenuContent>
