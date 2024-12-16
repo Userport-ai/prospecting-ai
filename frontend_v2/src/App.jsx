3487cc96b9fa70d2525b6f50d8c4d0d30249b3a6
add4user <addarsh@userport.ai>
2024-12-16 15:15:13 +0530

    Added ShadCN UI to project
    Looks great, let's go!

--- frontend_v2/src/App.jsx
+++ frontend_v2/src/App.jsx
@@ -1,8 +1,15 @@
+import { Button } from "./components/ui/button";
+
 function App() {
   return (
     <div className="flex flex-row justify-center items-center min-h-screen bg-slate-100 font-inter">
-      <div>
-        <p className="text-violet-600 font-semibold">Hello Userport</p>
+      <div className="flex flex-col gap-2">
+        <div>
+          <p className="text-violet-600 font-semibold text-center">
+            Hello Userport
+          </p>
+        </div>
+        <Button>ShadCN Button</Button>
       </div>
     </div>
   );
