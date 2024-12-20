7ab3cf877cd006710542f5fb1f18e8b0a27d8917
add4user <addarsh@userport.ai>
2024-12-20 23:05:28 +0530

    Converted visibility control to popover
    Added some formatting to make it look nicer

--- frontend_v2/src/accounts/DataTable.jsx
+++ frontend_v2/src/accounts/DataTable.jsx
@@ -18,18 +18,11 @@ import {
 } from "@/components/ui/table";
 import { Input } from "@/components/ui/input";
 import {
-  DropdownMenu,
-  DropdownMenuCheckboxItem,
-  DropdownMenuContent,
-  DropdownMenuTrigger,
-} from "@/components/ui/dropdown-menu";
-import {
   Popover,
   PopoverContent,
   PopoverTrigger,
 } from "@/components/ui/popover";
-
-import { CirclePlus } from "lucide-react";
+import { CirclePlus, Eye } from "lucide-react";
 import { Checkbox } from "@/components/ui/checkbox";
 import { Badge } from "@/components/ui/badge";
 
@@ -202,28 +195,31 @@ export function DataTable({ columns, data }) {
 
       {/* Column Visibility */}
       <div>
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="outline" className="ml-auto shadow-sm">
-              View
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="start">
+        <Popover>
+          <PopoverTrigger className="flex gap-2 items-center px-4 py-2 text-sm font-medium text-gray-600 rounded-md shadow-sm bg-white hover:bg-gray-100 transition duration-300">
+            <Eye size={18} />
+            <span>Visible Columns</span>
+          </PopoverTrigger>
+          <PopoverContent align="start" className="w-fit p-4">
             {table
               .getAllColumns()
               .filter((column) => column.getCanHide())
               .map((column) => (
-                <DropdownMenuCheckboxItem
+                <div
                   key={column.id}
-                  className="capitalize text-gray-700"
-                  checked={column.getIsVisible()}
-                  onCheckedChange={(value) => column.toggleVisibility(!!value)}
+                  className="flex items-center gap-4 p-2 rounded-md hover:bg-gray-100 transition duration-200"
                 >
-                  {column.id}
-                </DropdownMenuCheckboxItem>
+                  <Checkbox
+                    checked={column.getIsVisible()}
+                    onCheckedChange={(value) => column.toggleVisibility(value)}
+                  />
+                  <label className="flex justify-between items-center w-full text-sm text-gray-600">
+                    {column.id}
+                  </label>
+                </div>
               ))}
-          </DropdownMenuContent>
-        </DropdownMenu>
+          </PopoverContent>
+        </Popover>
       </div>
 
       {/* Table Container */}
