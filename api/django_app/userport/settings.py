3c12c30dad986e490ab510dd54bd0681770bce6a
sowrabh <sowrabh@userport.ai>
2025-01-29 23:20:51 +0530

    Disable request middleware

--- api/django_app/userport/settings.py
+++ api/django_app/userport/settings.py
@@ -62,7 +62,7 @@ MIDDLEWARE = [
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
     'app.middleware.TenantMiddleware',
-    'google.cloud.logging_v2.handlers.middleware.RequestMiddleware',  # Reference: https://cloud.google.com/python/docs/reference/logging/latest/web-framework-integration.
+#    'google.cloud.logging_v2.handlers.middleware.RequestMiddleware',  # Reference: https://cloud.google.com/python/docs/reference/logging/latest/web-framework-integration.
 ]
 
 # CORS Settings
