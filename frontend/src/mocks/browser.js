f4ba7f162a62b3b5bf74f48b3c76a55293deb4a3
add4user <addarsh@userport.ai>
2025-06-23 12:01:35 +0530

    Deleted frontend/
    V1 code which was stale

--- frontend/src/mocks/browser.js
+++
@@ -1,5 +0,0 @@
-import { setupWorker } from "msw/browser";
-import { handlers } from "./handlers";
-
-// Set up MockServiceWorker; see https://mswjs.io/docs/integrations/browser
-export const worker = setupWorker(...handlers);
